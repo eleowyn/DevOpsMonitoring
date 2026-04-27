04c8352b86220680ca29133b187dcbb9d338b0dc
Vitz15 <vitojonathan1515@gmail.com>
2026-04-28 00:18:28 +0800

    update gemini models

--- monitor.py
+++ monitor.py
@@ -10,7 +10,7 @@ load_dotenv()
 # Konfigurasi API Gemini
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
-model = genai.GenerativeModel(model_name="gemini-2.0-flash")
+model = genai.GenerativeModel(model_name="gemini-2.5-flash")
 
 def get_ssh_attempts():
     result = subprocess.check_output(
